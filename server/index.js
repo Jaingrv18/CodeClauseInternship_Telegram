01060377dfc3932ba5b71ab9ea1a703ca952ed35
Garv Jain <garvjain.1304@gmail.com>
2023-08-29 21:42:11 +0530

    File Changes

--- server/index.js
+++ server/index.js
@@ -7,20 +7,28 @@ const app = express();
 const socket = require("socket.io");
 const connectDB = require("./db");
 const path = require("path");
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 //--------------------Deployment----------------------------
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on ${process.env.PORT}`)
-);
+app.use(express.static(path.join(__dirname, "./public/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/build/index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
 const io = socket(server, {
   cors: {
     origin: "http://localhost:3000",
