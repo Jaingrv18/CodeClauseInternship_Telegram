225fff3cf8131daab207e8e800f11cc7a2d57676
Garv Jain <garvjain.1304@gmail.com>
2023-08-29 22:26:38 +0530

    File changes

--- server/index.js
+++ server/index.js
@@ -5,25 +5,29 @@ const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
-const connectDB = require("./db");
-const path = require("path");
-const dotenv = require("dotenv");
-dotenv.config();
-
-connectDB();
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-//--------------------Deployment----------------------------
-
-const PORT = process.env.PORT || 5000;
-
-const server = app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
-
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
 const io = socket(server, {
   cors: {
     origin: "http://localhost:3000",
